30148badd27da7b3bc2e8130b92eef6a8cadf42c
vg249 <vg249@cornell.edu>
2015-11-25 14:17:30 -0500

    FFT IFFT is working.Need to find a way to apply Gaussian mask. Need to check whther we can used Convolve

---
+++ CImg/CimgExample.cpp
@@ -0,0 +1,76 @@
+#include "CImg.h"
+#include <cmath>
+#include <string.h>
+#include <complex>
+
+#define PI 3.14159
+
+using namespace std;
+using namespace cimg_library;
+
+CImg<double> Multiply(CImg<double> Image){
+        printf("Multiply (-1)^(x+y) to center the transform for filtering\n");
+        CImg<double> MultiplyImage(Image.width(),Image.height(),1,1,0);
+        for ( int  i = 0; i < Image.width(); i++){
+                for ( int j = 0; j < Image.height(); j++)
+                        if ( (i + j)%2 != 0)
+                                MultiplyImage(i,j) = -Image(i,j);
+                        else
+                                MultiplyImage(i,j) = Image(i,j);
+
+        }
+        CImgDisplay MultiplyDisplay(MultiplyImage, "After (-1)^(x+y)");
+        system("PAUSE");
+        return MultiplyImage;
+
+}
+
+void Gaussian(CImg<double> Image){
+        printf("Construct Gaussian highpass filter\n");
+        complex<double> H[100][100];
+        double D0, D;
+        printf("Please input radii:");
+        scanf("%lf", &D0);
+        for ( int u = 0; u < Image.width() ; u++){
+                for ( int v = 0; v < Image.height() ; v++){
+                        D = sqrt(pow((double)u - ((double)Image.width()/2),2) + pow((double)v - ((double)Image.height()/2),2));
+                        H[u][v] =(complex<double>)(1,0.0)- exp(complex<double>(0.0,-(double)((double)pow(D, 2)/ (double)(2* pow(D0,2)))));
+                }
+        }
+
+}
+
+
+int main() {
+  
+  const CImg<double> Originalimg = CImg<double>("marilyn1.png").resize(256,256);
+  CImg<double> img = Multiply(Originalimg);
+
+  CImgList<double> F = img.get_FFT();
+//  cimglist_apply(F,shift)(img.width()/2,img.height()/2,0,0,2);
+  //CImgList<float> F = FT.get_FFT(true);
+
+  complex<double> H[256][256];
+  double D0,D;
+
+  for ( int u = 0; u < img.width() ; u++){
+    for ( int v = 0; v < img.height() ; v++){
+      D = sqrt(pow((double)u - ((double)img.width()/2),2) + pow((double)v - ((double)img.height()/2),2));
+      H[u][v] =(complex<double>)(1,0.0)- exp(complex<double>(0.0,-(double)((double)pow(D, 2)/ (double)(2* pow(D0,2)))));
+       }
+   }
+
+   for ( int u = 0; u < img.width() ; u++){
+	for ( int v = 0; v < img.height() ; v++)
+	    F[u][v] *= H[u][v];
+	}
+
+  const CImg<double> mag = ((F[0].get_pow(2) + F[1].get_pow(2)).sqrt() + 1).log().normalize(0,255);
+ //const CImg<unsigned char> mag = ((F[0].get_pow(2) + F[1].get_pow(2)).sqrt() + 1);
+ //const CImg<unsigned char> mag = ((FT[0].get_pow(2) + FT[1].get_pow(2)).sqrt() + 1);
+  CImgList<double> visu(img,mag);
+  mag.save("fftimage.png");
+  
+}
+
+
--- ImageProcessing.cpp
+++ ImageProcessing.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstdio>
+#include <complex>
 #define FALSE 0;
 #define TRUE 1;
 
@@ -109,11 +110,108 @@ int main()
 	size_t result;
         result = fread(image,1,totalPixels,fpIn);
         for(int i=0;i<totalPixels;i++)  
-	{	
-                printf("0x%x\n",image[i]);
+	{
+			
+                printf("%d\n",image[i]);
             
 	}
         printf("0x%x \n", result); 
 	cout<<"Reading the image "<<"of zhiru "<<" was sucessfull...\n";
         free(image);
+}
+
+int FFT(int dir,int m,double *x,double *y)
+{
+   long nn,i,i1,j,k,i2,l,l1,l2;
+   double c1,c2,tx,ty,t1,t2,u1,u2,z;
+
+   /* Calculate the number of points */
+   nn = 1;
+   for (i=0;i<m;i++)
+      nn *= 2;
+
+   /* Do the bit reversal */
+   i2 = nn >> 1;
+   j = 0;
+   for (i=0;i<nn-1;i++) {
+      if (i < j) {
+         tx = x[i];
+         ty = y[i];
+         x[i] = x[j];
+         y[i] = y[j];
+         x[j] = tx;
+         y[j] = ty;
+      }
+      k = i2;
+      while (k <= j) {
+         j -= k;
+         k >>= 1;
+      }
+      j += k;
+   }
+
+   /* Compute the FFT */
+   c1 = -1.0;
+   c2 = 0.0;
+   l2 = 1;
+   for (l=0;l<m;l++) {
+      l1 = l2;
+      l2 <<= 1;
+      u1 = 1.0;
+      u2 = 0.0;
+      for (j=0;j<l1;j++) {
+         for (i=j;i<nn;i+=l2) {
+            i1 = i + l1;
+            t1 = u1 * x[i1] - u2 * y[i1];
+            t2 = u1 * y[i1] + u2 * x[i1];
+            x[i1] = x[i] - t1;
+            y[i1] = y[i] - t2;
+            x[i] += t1;
+            y[i] += t2;
+         }
+         z =  u1 * c1 - u2 * c2;
+         u2 = u1 * c2 + u2 * c1;
+         u1 = z;
+      }
+      c2 = sqrt((1.0 - c1) / 2.0);
+      if (dir == 1)
+         c2 = -c2;
+      c1 = sqrt((1.0 + c1) / 2.0);
+   }
+
+   /* Scaling for forward transform */
+   if (dir == 1) {
+      for (i=0;i<nn;i++) {
+         x[i] /= (double)nn;
+         y[i] /= (double)nn;
+      }
+   }
+
+   return(TRUE);
+}
+
+/*-------------------------------------------------------------------------
+   Calculate the closest but lower power of two of a number
+   twopm = 2**m <= n
+   Return TRUE if 2**m == n
+*/
+int Powerof2(int n,int *m,int *twopm)
+{
+   if (n <= 1) {
+      *m = 0;
+      *twopm = 1;
+      return(FALSE);
+   }
+
+   *m = 1;
+   *twopm = 2;
+   do {
+      (*m)++;
+      (*twopm) *= 2;
+   } while (2*(*twopm) <= n);
+
+   if (*twopm != n)
+      return(FALSE);
+   else
+      return(TRUE);
 }
