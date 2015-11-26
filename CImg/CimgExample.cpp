9870ba878f0c34557c594c6d04e570943dc6215b
vg249 <vg249@cornell.edu>
2015-11-26 10:50:45 -0500

    11262015 Code

--- CImg/CimgExample.cpp
+++ CImg/CimgExample.cpp
@@ -1,5 +1,6 @@
 #include "CImg.h"
 #include <cmath>
+#include <iostream>
 #include <string.h>
 #include <complex>
 
@@ -43,7 +44,7 @@ void Gaussian(CImg<double> Image) {
 
 int main() {
   
-  const CImg<double> img = CImg<double>("marilyn1.png").resize(256,256);
+  const CImg<double> img = CImg<double>("marilyn1.png").resize(256,256).save("bharath.png");
 //  CImg<double> img = Multiply(Originalimg);
 
   CImgList<double> F = img.get_FFT();
@@ -53,25 +54,33 @@ int main() {
 
   complex<double> H[256][256];
   double D0,D;
+ 
+  double B[65536];
+  double S[65536];  
 
+  int i = 0;
   for ( int u = 0; u < img.width() ; u++){
     for ( int v = 0; v < img.height() ; v++){
+      D0 = 10;
       D = sqrt(pow((double)u - ((double)img.width()/2),2) + pow((double)v - ((double)img.height()/2),2));
-      H[u][v] =(complex<double>)(1,0.0)- exp(complex<double>(0.0,-(double)((double)pow(D, 2)/ (double)(2* pow(D0,2)))));
+      H[u][v] = complex<double>(1,0.0) -  exp(complex<double>(0.0,-(double)((double)pow(D, 2)/ (double)(2* pow(D0,2)))));
+      B[i] = std::real(H[u][v]);
+      S[i] = std::imag(H[u][v]);     
+      i++;
        }
    }
-//complex<double> S[][];
-  // for ( int u = 0; u < img.width() ; u++){
-//	for ( int v = 0; v < img.height() ; v++)
-//	   printf("%f", F[u][v]);
-//*= H[u][v];
-//	}
+
+ printf("%d",i);
+ for (int z=0; z< (img.width()*img.height()); z++)
+ {
+  F[0][z] = F[0][z]*B[i]; 
+  F[1][z] = F[1][z]*S[i];
+ }  
   
+
   CImgList<double> FT = F.get_FFT(true);
-//  printf("%f",H[0][0]);
+//  const CImg<double> mag = ((FT[0].get_pow(2) + FT[1].get_pow(2)).sqrt() + 1).log().normalize(0,255);
   const CImg<double> mag = ((FT[0].get_pow(2) + FT[1].get_pow(2)).sqrt() + 1).log().normalize(0,255);
- //const CImg<unsigned char> mag = ((F[0].get_pow(2) + F[1].get_pow(2)).sqrt() + 1);
-//const CImg<unsigned char> mag = ((FT[0].get_pow(2) + FT[1].get_pow(2)).sqrt() + 1);
   CImgList<double> visu(img,mag);
   mag.save("fftimage.png");
   
