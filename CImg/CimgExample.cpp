0031a1462249fed5fd3c3fbc217e55ad02f55990
vg249 <vg249@cornell.edu>
2015-11-26 16:36:10 -0500

    Gaussian Filtering is not woring

--- CImg/CimgExample.cpp
+++ CImg/CimgExample.cpp
@@ -9,78 +9,52 @@
 using namespace std;
 using namespace cimg_library;
 
-CImg<double> Multiply(CImg<double> Image){
-        printf("Multiply (-1)^(x+y) to center the transform for filtering\n");
-        CImg<double> MultiplyImage(Image.width(),Image.height(),1,1,0);
-        for ( int  i = 0; i < Image.width(); i++){
-                for ( int j = 0; j < Image.height(); j++)
-                        if ( (i + j)%2 != 0)
-                                MultiplyImage(i,j) = -Image(i,j);
-                        else
-                                MultiplyImage(i,j) = Image(i,j);
-
-        }
-        CImgDisplay MultiplyDisplay(MultiplyImage, "After (-1)^(x+y)");
-        system("PAUSE");
-        return MultiplyImage;
-
-}
-
-void Gaussian(CImg<double> Image) {
-        printf("Construct Gaussian highpass filter\n");
-        complex<double> H[100][100];
-        double D0, D;
-        printf("Please input radii:");
-        scanf("%lf", &D0);
-        for ( int u = 0; u < Image.width() ; u++){
-                for ( int v = 0; v < Image.height() ; v++){
-                        D = sqrt(pow((double)u - ((double)Image.width()/2),2) + pow((double)v - ((double)Image.height()/2),2));
-                        H[u][v] =(complex<double>)(1,0.0)- exp(complex<double>(0.0,-(double)((double)pow(D, 2)/ (double)(2* pow(D0,2)))));
-                }
-        }
-
-}
-
 
 int main() {
-  
-  const CImg<double> img = CImg<double>("marilyn1.png").resize(256,256).save("bharath.png");
-//  CImg<double> img = Multiply(Originalimg);
+ 
+  //Reading the image 
+  const CImg<double> img = CImg<double>("marilyn1.png").resize(256,256).save("original.png");
 
+  //Applying fourier transform. Referenced it frm CImg.h. 
+  //Returns list in 0 and 1 column. We assummed the values in 0 column are magnitude and 1 column are phase
   CImgList<double> F = img.get_FFT();
+
+  //FFT Shift. Referenced from CImg.h
   cimglist_apply(F,shift)(img.width()/2,img.height()/2,0,0,2);
   
-//CImgList<float> F = FT.get_FFT(true);
+  complex<double> H[256][256];   //Complex double array for saving Gaussian mask
 
-  complex<double> H[256][256];
   double D0,D;
- 
-  double B[65536];
+  double B[65536];   //65536 is the total number pixels available in the image
   double S[65536];  
 
+ //Calculating the gaussian mask. Magnitude and the Phase values are saved in seperate arrays.
+ //Referenced from Online source. The mask is for low pass filter.
   int i = 0;
   for ( int u = 0; u < img.width() ; u++){
     for ( int v = 0; v < img.height() ; v++){
-      D0 = 10;
+      D0 = 15;
       D = sqrt(pow((double)u - ((double)img.width()/2),2) + pow((double)v - ((double)img.height()/2),2));
-      H[u][v] = complex<double>(1,0.0) -  exp(complex<double>(0.0,-(double)((double)pow(D, 2)/ (double)(2* pow(D0,2)))));
-      B[i] = std::real(H[u][v]);
-      S[i] = std::imag(H[u][v]);     
+      H[u][v] =  exp(complex<double>(0.0,-(double)((double)pow(D, 2)/ (double)(2* pow(D0,2)))));
+      B[i] = std::abs(H[u][v]);
+      S[i] = std::arg(H[u][v]); 
       i++;
+
        }
    }
 
- printf("%d",i);
- for (int z=0; z< (img.width()*img.height()); z++)
+ printf("%d",i); 
+//Multiplying the Magnitude of Gaussian Mask with Magnitude of FFT result
+ for (int z=0; z< i; z++)
  {
-  F[0][z] = F[0][z]*B[i]; 
-  F[1][z] = F[1][z]*S[i];
+  F[0][z] = (F[0][z]*B[i]); 
  }  
   
-
+//Taking Inverse FFT of the Result
   CImgList<double> FT = F.get_FFT(true);
-//  const CImg<double> mag = ((FT[0].get_pow(2) + FT[1].get_pow(2)).sqrt() + 1).log().normalize(0,255);
+
   const CImg<double> mag = ((FT[0].get_pow(2) + FT[1].get_pow(2)).sqrt() + 1).log().normalize(0,255);
+
   CImgList<double> visu(img,mag);
   mag.save("fftimage.png");
   
