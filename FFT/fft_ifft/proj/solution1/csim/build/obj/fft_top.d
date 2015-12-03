82b721665bd138b603b7c92242549be04d579334
vg249 <vg249@cornell.edu>
2015-12-02 22:14:17 -0500

    fft_tb modified

---
+++ FFT/fft_ifft/proj/solution1/csim/build/obj/fft_top.d
@@ -0,0 +1,10 @@
+obj/fft_top.o: ../../../../fft_top.cpp ../../../../fft_top.h \
+ /opt/xilinx/Vivado_HLS/2014.2/include/ap_fixed.h \
+ /opt/xilinx/Vivado_HLS/2014.2/include/ap_int.h \
+ /opt/xilinx/Vivado_HLS/2014.2/include/etc/ap_int_sim.h \
+ /opt/xilinx/Vivado_HLS/2014.2/include/etc/ap_private.h \
+ /opt/xilinx/Vivado_HLS/2014.2/include/etc/ap_fixed_sim.h \
+ /opt/xilinx/Vivado_HLS/2014.2/include/etc/ap_int_sim.h \
+ /opt/xilinx/Vivado_HLS/2014.2/include/hls_fft.h \
+ /opt/xilinx/Vivado_HLS/2014.2/include/hls_stream.h \
+ /opt/xilinx/Vivado_HLS/2014.2/include/fft/xfft_v9_0_bitacc_cmodel.h
---
+++ FFT/fft_ifft/proj/vivado_hls.app
@@ -0,0 +1,16 @@
+<project xmlns="com.autoesl.autopilot.project" name="proj" top="fft_top">
+    <libraryPaths/>
+    <Simulation argv="">
+        <SimFlow name="csim" ldflags="" mflags="" setup="false" optimizeCompile="false" clean="false"/>
+    </Simulation>
+    <includePaths xmlns=""/>
+    <libraryFlag xmlns=""/>
+    <files xmlns="">
+        <file name="../../fft_tb.cpp" sc="0" tb="1" cflags=" "/>
+        <file name="fft_top.cpp" sc="0" tb="false" cflags=""/>
+    </files>
+    <solutions xmlns="">
+        <solution name="solution1" status=""/>
+    </solutions>
+</project>
+
