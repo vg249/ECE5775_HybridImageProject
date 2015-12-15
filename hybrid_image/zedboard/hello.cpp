b229528590f683273fbbbecc504959933f9aa5f7
Zhang Group <zhang-group@localhost.localdomain>
2015-12-15 13:09:23 -0500

    add a compilation test

---
+++ hybrid_image/zedboard/hello.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+//#include<unistd.h>
+//#include<fcntl.h>
+//#include<hls_math.h>
+#include<assert.h>
+#include <complex>
+
+#include<iostream>
+#include<fstream>
+
+//#include "CImg.h"
+//#include "timer.h"
+
+using namespace std;
+//typedef ap_uint<32> bit32_t;
+
+//--------------------------------------
+// main function
+//--------------------------------------
+int main(int argc, char** argv)
+{
+  cout<<"hello"<<endl;
+  return 1;
+}
